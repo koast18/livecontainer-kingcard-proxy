46644a11f3f377f710b52f5a44bc5432712af0fe
koast18 <koast18@users.noreply.github.com>
2026-08-17 04:27:39 +0800

    feat: generate richer proxychains.conf based on LiveContainer template

--- Tweak/Sources/LCProxyServer.m
+++ Tweak/Sources/LCProxyServer.m
@@ -11,7 +11,7 @@
 #import "GCDWebServerDataResponse.h"
 #import "GCDWebServerRequest.h"
 
-static NSString *const LCProxyVersion = @"0.3.5";
+static NSString *const LCProxyVersion = @"0.3.6";
 static const NSUInteger LCProxyDefaultPort = 19092;
 
 @interface LCProxyServer ()
