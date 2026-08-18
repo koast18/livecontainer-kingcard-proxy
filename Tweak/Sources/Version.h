ee3039653fe932665e779087049734ba18d0422b
koast18 <koast18@users.noreply.github.com>
2026-08-18 12:31:07 +0800

    release: bump version to 0.5.8

--- Tweak/Sources/Version.h
+++ Tweak/Sources/Version.h
@@ -1,5 +1,5 @@
 #ifndef LCProxy_Version_h
 #define LCProxy_Version_h
-#define KPTWEAK_VERSION "0.5.7"
-#define KPTWEAK_UA "LCProxy/0.5.7"
+#define KPTWEAK_VERSION "0.5.8"
+#define KPTWEAK_UA "LCProxy/0.5.8"
 #endif
