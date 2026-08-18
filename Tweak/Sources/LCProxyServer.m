fad6034d03781fabb210eaab21701979f64c773f
koast18 <koast18@users.noreply.github.com>
2026-08-18 11:49:15 +0800

    release: bump version to 0.5.5

--- Tweak/Sources/LCProxyServer.m
+++ Tweak/Sources/LCProxyServer.m
@@ -13,7 +13,7 @@
 #import "GCDWebServerDataResponse.h"
 #import "GCDWebServerRequest.h"
 
-static NSString *const LCProxyVersion = @"0.5.4";
+static NSString *const LCProxyVersion = @"0.5.5";
 static const NSUInteger LCProxyDefaultPort = 19092;
 
 @interface LCProxyServer ()
--- Tweak/Sources/Version.h
+++ Tweak/Sources/Version.h
@@ -1,5 +1,5 @@
 #ifndef LCProxy_Version_h
 #define LCProxy_Version_h
-#define KPTWEAK_VERSION "0.5.4"
-#define KPTWEAK_UA "LCProxy/0.5.4"
+#define KPTWEAK_VERSION "0.5.5"
+#define KPTWEAK_UA "LCProxy/0.5.5"
 #endif
